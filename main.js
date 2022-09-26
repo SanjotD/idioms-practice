43dc85fdf4841f34ca7e7108f7d6e4c1599c8911
SanjotD <112981867+SanjotD@users.noreply.github.com>
2022-09-26 14:05:49 -0600

    finsihed

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
 // JavaScript Code
+
+document.getElementById("btn1").addEventListener("click", devil)
+document.getElementById("btn2").addEventListener("click", bLeg)
+document.getElementById("btn3").addEventListener("click", good)
+document.getElementById("btn4").addEventListener("click", tog)
+
+function devil () {
+    alert ("devil.")
+}
+
+function bLeg () {
+    alert ("leg.")
+}
+
+function good () {
+    alert ("as good as mine.")
+
+}function tog () {
+    alert ("together.")
+}
